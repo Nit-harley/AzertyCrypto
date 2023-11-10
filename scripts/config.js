bf6c11d393dc6433331510fc156ec58a744fb749
Nit-Harley <harleynit63@gmail.com>
2023-11-10 19:18:28 +0100

    Ajout des fichiers

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Blockchain", "Bitcoin", "Smartcontract", "Ethreum", "Cardano", "Gugucoin"]
+const listePhrases = ["Bitcoin est né en 2009 !", "La Cryptomonnaie est basée sur la blockhain", "Le satoshi est la plus petite unité de Bitcoin"]
---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let spanZoneScore = document.querySelector(".zoneScore span")
+console.log(spanZoneScore)
+
+let listeBtnRadio = document.querySelectorAll(".optionSource input")
+console.log(listeBtnRadio)
+
